1f024ce5344c7f2c8ba64f32f757a0f464673d9e
Ujwal Gowda V <ujwalgowda2002v@gmail.com>
2026-04-19 16:44:31 +0530

    Why msg

--- src/App.jsx
+++ src/App.jsx
@@ -45,8 +45,6 @@ export default function App() {
           <br /><br />
           and remembered forever ✨
         </p>
-        <br /><br />
-        <p>And yes I won't be available for few weeks, and I won't be available for calls too</p>
       </div>
     </motion.div>
   );
